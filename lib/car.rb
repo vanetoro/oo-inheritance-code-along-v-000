2c5e302df9e2a352c144a6ad49e99a913cb35937
Vanessa Toro <vanetoro84@gmail.com>
2018-04-04 15:05:20 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+
+class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+end
